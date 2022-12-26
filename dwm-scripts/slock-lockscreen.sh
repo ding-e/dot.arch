0a72f820b41204b23383dff12f712d17c66debd6
ding-e <2016669264@qq.com>
2022-12-26 11:39:31 +0800

    清理

--- dwm-scripts/slock-lockscreen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# slock -m "Locked at $(date "+%Y.%m.%d %H:%M")"
-firejail --net=none slock -m "Locked at $(date "+%Y.%m.%d %H:%M")"
--- dwm-scripts/wp-change.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# feh --recursive --randomize --bg-fill /home/dinge/core/picture/wallpapers/view
-# feh --recursive --randomize --bg-fill /home/dinge/core/picture/wallpapers/change
-firejail --net=none feh --recursive --randomize --bg-fill /home/dinge/core/picture/wallpapers/change
